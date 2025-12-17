e34ca197d40178ce56d1cc48ecac56b65c8f0445
Vladimir030696 <vova.eremeev.96@inbox.ru>
2025-12-17 10:30:26 +0900

    Модульный JS и npm

---
+++ webdev-dom-homework-main/list.js
@@ -0,0 +1,18 @@
+ import {rendorStudion} from"./rendorStudion.js";
+ import { lik } from "./masiv.js";
+ export const listModule =()=>{
+ const textEl = document.getElementById("tex")
+  const nameEl = document.getElementById("field")
+  list.addEventListener('click', (event) => {
+   if (event.target.classList.contains('comment-text')) {
+     const index = parseInt(event.target.dataset.index);
+     const author = lik[index].name;
+     const text = lik[index].text;   
+     const quote = `Автор: ${author}\n
+     \n Ответ на: ${text}
+     \n`;
+     textEl.value = quote + textEl.value;
+     nameEl.focus();
+   }
+ });}
+   rendorStudion()
